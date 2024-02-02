5e787dc2b1b518d36a5ee630430a071dfacbc510
Carlos Ival <carlos.martinez@evvohome.com>
2024-02-02 12:06:51 +0100

    Nest as CLI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CliModule } from './framework/cli/cli.module';
-import { Cli2Module } from './framework/cli2/cli2.module';
 
 @Module({
   imports: [CliModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
