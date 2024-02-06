7312e6dd4d4987c3188e6aac9ad4dc4e943d075e
Carlos Ival <carlos.martinez@evvohome.com>
2024-02-06 10:10:45 +0100

    Fixing Presentation formatting table

--- src/framework/presentation-service/presentation.service.imp.ts
+++ src/framework/presentation-service/presentation.service.imp.ts
@@ -1,5 +1,7 @@
 import { IClientUseCasesOUT } from '../../core/abstracts/client.use-cases';
 import { ClientSuspiciousDto } from '../../core/dtos/client.suspicious.dto';
+import { Transform } from 'stream';
+import { Console } from 'console';
 
 export class PresentationService implements IClientUseCasesOUT {
   printSuspicious(suspicious: ClientSuspiciousDto[]): void {
@@ -7,13 +9,27 @@ export class PresentationService implements IClientUseCasesOUT {
   }
 
   private printSus(suspiciousClients: ClientSuspiciousDto[]): void {
-    // Print the table to the console
-    console.log('| Client | Month | Suspicious | Median'); // PadEndMAx String of every column
-    console.log('----------------------------------');
-    suspiciousClients.forEach((client) => {
-      console.log(
-        `| ${client.id} | ${client.month} | ${client.suspicious} | ${client.median} `,
-      );
+    this.table(suspiciousClients);
+  }
+  private table(input: ClientSuspiciousDto[]) {
+    // @see https://stackoverflow.com/a/67859384
+    const ts = new Transform({
+      transform(chunk, enc, cb) {
+        cb(null, chunk);
+      },
     });
+    const logger = new Console({ stdout: ts });
+    logger.table(input);
+    const table = (ts.read() || '').toString();
+    let result = '';
+    for (const row of table.split(/[\r\n]+/)) {
+      let r = row.replace(/[^┬]*┬/, '┌');
+      r = r.replace(/^├─*┼/, '├');
+      r = r.replace(/│[^│]*/, '');
+      r = r.replace(/^└─*┴/, '└');
+      r = r.replace(/'/g, ' ');
+      result += `${r}\n`;
+    }
+    console.log(result);
   }
 }
